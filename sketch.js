757a0f8af1b3757e03673e1030a197ad3807b992
uthred17 <46094267+uthred17@users.noreply.github.com>
2019-09-25 22:36:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,29 @@
-function setup() {
-  createCanvas(800, 800);
+//var leftEyeX = 500;
 
-  
+function setup(){
+  createCanvas(800, 800);
+  smooth();
+  frameRate(20);
+  //background(100);
 }
 
+
+
 function draw(){
-  background(199, 199, 199);
+  background(100);
+  
+  for(let x = 30; x < width; x += 80){
+    for(let y = 30; y < height; y += 80){
+      
+      //drawEye(100, 400);
+      
+        var skin = color('	#CD853F');
   noStroke();
   
-   var skin = color('	#CD853F');
-  noStroke();
-  ellipseMode(CENTER);
+  let x1 = map(mouseX, 0, width, 100, 75);
+  let x2 = map(mouseX, 0, width, -17, 17, true);
   
- 
-  
-    // face
+      // face
   noStroke();
   fill(skin);
   ellipse(width/2, height/2, 500, 600);
@@ -35,8 +44,8 @@ ellipse(400, 181, 343, 173);
   ellipse(300, 420, 50, 50);
   ellipse(500, 420, 50, 50);
   fill('#111');
-  ellipse(300, 420, 15, 15);
-  ellipse(500, 420, 15, 15);
+  ellipse(500+x2, 420, 15, 15);
+  //ellipse(500, 420, 15, 15);
   
   // nose
   noStroke();
@@ -57,8 +66,52 @@ ellipse(400, 181, 343, 173);
   ellipse(15, 41, 5, 10);
   ellipse(650, 410, 50, 100);
   ellipse(150, 410, 50, 100);
+      
+   drawEye(300, 420);
   
-  println("mouseX="+mouseX+", mouseY="+mouseY);
+  console.log("mouseX="+mouseX+", mouseY="+mouseY);
+        
+
+    }
+    
+    
+    
+  }
+  
+        
   
 
+        
+ 
+ 
+ 
+   
+
+    }
+//Blink function
+function drawEye(x, y){
+
+
+  
+  
+  fill(255);
+  ellipse(x, y, 30, 30);
+
+  if(random(10) < 9){
+    fill(0);
+   
+    ellipse(x + 1, y +1, 7, 7);
+  }
+  else {
+    
+    stroke(0);
+    line(x-1, y, x + 1, y);    
+  }
 }
+
+
+
+
+  
+
+  
